6d4983250b59c0ee74b729fc3c11a05f23df17c7
ericcanton <57836255+ericcanton@users.noreply.github.com>
2020-05-27 14:38:19 -0400

    evaluatory.py initial commit.

---
+++ source/evaluator.py
@@ -0,0 +1,119 @@
+import numpy as np
+
+"""
+data should be a list of lists with structure:
+    -- data[0] = training data
+    -- data[1] = testing data
+    each data[i] has two elements:
+    -- data[i][0] = model inputs. Either spectrogram tensor, or [spectrogram, times] 
+    -- data[i][1] = PSG labels tensor. DO NOT PREPROCESS THIS INTO BI/TRINARY CLASSES 
+
+nn_model should be a compiled Keras model, either trained or untrained. 
+For each d in data_splits, a ("deep") copy will be made and trained for 15 epochs. 
+"""
+def train_then_ROC_PR(data, nn_model, uses_time = True, saveto_path = None, pos_label = 0):
+    if not (isinstance(pos_label, int) or isinstance(pos_label, list)):
+        raise ValueError("pos_label needs to be an integer or a list of length 3 or 4.")
+        return None
+
+    fprs = []
+    tprs = []
+    precs = []
+    recas = []
+    aucs = []
+
+    training, testing  = data
+
+    #####################################################################
+    # Discard -1 ("unscored") labels, and corresponding data
+    #####################################################################
+    training_scored, testing_scored = (training[1] != -1), (testing[1] != -1)
+
+    # Do PSG labels first
+    training[1] = training[1][training_scored]
+    testing[1] = testing[1][testing_scored]
+
+    # Now reduce the input data accordingly
+    if uses_time:
+        # spectrograms
+        training[0][0] = training[0][0][training_scored]
+        testing[0][0] = testing[0][0][testing_scored]
+
+        # time
+        training[0][1] = training[0][1][training_scored]
+        testing[0][1] = testing[0][1][testing_scored]
+    else:
+        training[0] = training[0][training_scored]
+        testing[0] = testing[0][testing_scored]
+
+    #####################################################################
+    # Now modify the labels to give wake/sleep or staging 
+    #####################################################################
+
+    # Decide which label mode the user wants based on the type, and possibly length, of pos_label. 
+    if isinstance(pos_label, int): # binary classifier (Awake/Asleep), default
+        training[1] = np.where(training[1] > 0, 1, 0)
+        testing[1] = np.where(testing[1] > 0, 1, 0)
+        mode = 2
+
+    elif isinstance(pos_label, list):
+        if len(pos_label) == 3: # Awake/NREM/REM
+            training[1] = np.piecewise( \
+                training[1], \
+                [ \
+                 training[1] == 0, \
+                 (0 < training[1]) & (training[1] < 5), \
+                 training[1] == 5 \
+                ], \
+                [0,1,2] \
+            )
+            testing[1] = np.piecewise( \
+                testing[1], \
+                [ \
+                 testing[1] == 0, \
+                 (0 < testing[1]) & (testing[1] < 5), \
+                 testing[1] == 5 \
+                ], \
+                [0,1,2] \
+            )
+            mode = 3
+        else: # Awake/NREM12/NREM34/REM
+            training[1] = np.piecewise(
+                training[1], \
+                [training[1] == 0, \
+                 (0 < training[1]) & (training[1] < 3), \
+                 (2 < training[1]) & (training[1] < 5), \
+                 training[1] == 5
+                ], \
+                [0, 1, 2, 3] \
+            )
+            testing[1] = np.piecewise(
+                testing[1], \
+                [testing[1] == 0, \
+                 (0 < testing[1]) & (testing[1] < 3), \
+                 (2 < testing[1]) & (testing[1] < 5), \
+                 testing[1] == 5
+                ], \
+                [0, 1, 2, 3] \
+            )
+            mode = 4
+
+    print("Split okay! Using %d labels." % mode)
+    print(training[1])
+    print(testing[1])
+    return (fprs, tprs, aucs, precs, recas)
+
+    this_nn = deepcopy(nn_model)
+
+    this_nn.fit(training[0], testing, epochs=15, verbose = 0)
+
+    predictions = this_nn.predict(testing)
+
+if __name__ == "__main__":
+    training = [np.arange(0, 10, 1), np.random.randint(-1, 6, 10)]
+    testing = [np.arange(0, 5, 1), np.random.randint(-1, 6, 5)]
+    
+    print(training[1])
+    print(testing[1])
+
+    train_then_ROC_PR([training, testing], "abc", uses_time=False, pos_label=[0,1,2])
