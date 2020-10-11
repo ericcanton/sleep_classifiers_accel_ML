5ad727209679caadf12524d7f90fe03ecb23ec1c
ericcanton <57836255+ericcanton@users.noreply.github.com>
2020-10-11 15:38:50 -0500

    Refactor of evalution effort: pr_roc function written. Now to use this in bulk.

--- source/evaluator.py
+++ source/evaluator.py
@@ -1,11 +1,11 @@
 """ 
 Evaluation scripts. There are four functions defined here:
-    1. logits_to_proba @ line 34
-    2. data_yielder @ line 42
-    3. split_yielder @ line  92
-    4. pr_roc_from_path @ line 145
-    5. sample_pics @ line 279
-    6. get_probabilities @ line 270
+    1. logits_to_proba
+    2. data_yielder
+    3. split_yielder
+    4. pr_roc_from_path
+    5. sample_pics
+    6. get_probabilities
 """
 
 import numpy as np
@@ -19,26 +19,41 @@ from sklearn.metrics import roc_curve, precision_recall_curve, auc
 import os
 import pickle
 
-"""
-Parameters: logit, logarithmic odds of probability p.
-    By definition, logits are:
-      logit(p) = log(p/(1-p)).
-    logits are easily transformed into a probability:
-      p = exp(logit)/(1 + exp(logit)).
-    Logits are useful when training classifier models because they do not 
-    suffer from some of the numerical issues that the sigmoid function 
-    does. A key issue is saturation:  if \sigma(x) is the sigmoid, then 
-      (d/dx)\sigma(x) is very small for |x| >>0, 
-    leading to numerical underflow that prevents gradient descent methods 
-    from being effective in training classifiers. 
-
-Values: probability of given logit
-    p(logit) = exp(logit)/(1+exp(logit))
-"""    
 def logits_to_proba(logit):
+    """
+    Parameters: logit, logarithmic odds of probability p.
+        By definition, logits are:
+          logit(p) = log(p/(1-p)).
+        logits are easily transformed into a probability:
+          p = exp(logit)/(1 + exp(logit)).
+        Logits are useful when training classifier models because they do not 
+        suffer from some of the numerical issues that the sigmoid function 
+        does. A key issue is saturation:  if \sigma(x) is the sigmoid, then 
+          (d/dx)\sigma(x) is very small for |x| >>0, 
+        leading to numerical underflow that prevents gradient descent methods 
+        from being effective in training classifiers. 
+    
+    Values: probability of given logit
+        p(logit) = exp(logit)/(1+exp(logit))
+    """    
     return np.exp(logit)/(1+np.exp(logit))
 
 def sleep_classes(inputs, n_classes):
+    """
+    Convert 6-stage classification to n_classes.
+    The six stages are:
+        0: awake
+        1: nrem1
+        2: nrem2
+        3: nrem3
+        4: nrem4
+        5: rem
+
+    For our purposes, only makes sense to group these as follows:
+        2 stage: sleep 0, wake 1
+        3 stage: sleep 0, nrem 1, rem 2
+        4 stage: sleep 0, light nrem 1, deep nrem 2, rem 3
+    """
     if n_classes == 2:
         return np.where(inputs > 0, 1, 0)
     elif n_classes == 3:
@@ -50,17 +65,20 @@ def sleep_classes(inputs, n_classes):
                                      (inputs > 0) & (inputs < 3),
                                      (inputs >= 3) & (inputs < 5),
                                      inputs == 5], [0, 1, 2, 3])
+    else:
+        raise ValueError("n_classes must be 2, 3 or 4.")
 
-
-# Generator to create leave-one-out train/test splits.
-# Upon initialization, loads the separate data pickles.
-# Yields: (subject_number[i], spectrogram[i], time[i], psg labels[i], neural network[i])
-## If exclude != None, then it's a list of subject numbers not to be yielded.
 def data_yielder(data_path, neural_path = None, exclude = None, nn = True, nn_base_name = None):
+    """
+     Generator to create leave-one-out train/test splits.
+     Upon initialization, loads the separate data pickles.
+     Yields: (subject_number[i], spectrogram[i], time[i], psg labels[i], neural network[i])
+        If exclude != None, then it's a list of subject numbers not to be yielded.
+    """
     # Ensure uniform input
     data_path = data_path if data_path[-1] == "/" else data_path + "/"
 
-    if neural_path == None:
+    if neural_path is None:
         neural_path = data_path + "neural/"
     neural_path = neural_path if neural_path[-1] == "/" else neural_path + "/"
     pickle_path = data_path + "pickles/"
@@ -79,7 +97,8 @@ def data_yielder(data_path, neural_path = None, exclude = None, nn = True, nn_ba
     # next(os.walk)[1] is the list of files in next(os.walk)[0] 
     #   (not folders, which is next(os.walk)[2])
     list_of_subjects = next(os.walk(pickle_path))[1]
-    list_of_subjects = [s for s in list_of_subjects if s not in exclude]
+    if exclude is not None:
+        list_of_subjects = [s for s in list_of_subjects if s not in exclude]
 
     for s in list_of_subjects:
         
@@ -98,25 +117,20 @@ def data_yielder(data_path, neural_path = None, exclude = None, nn = True, nn_ba
         else:
             yield list_of_subjects[i], spectros[i], time[i], psg[i]
         
-# See data_yielder for an explanation of the parameters.
-# Yields: (subject number excluded from training split, training_split_data, testing_split_data)
-## Where:
-# training_split_data:
-#   [0] if with_time, a 2-tuple:
-#     [0] training spectrograms tensor, shape (*, 65, 73)
-#     [1] training time tensor, shape (*,)
-#     (!) if not with_time, just element [0]
-#   [1] training psg labels tensor, shape (*,)
-# testing_split_data is similar.
 def split_yielder(data_path = None, exclude = None, with_time = True):
-            
     """
-    Each d in ev.data_yielder(...) is a 4-tuple:
-      [0] this subject's number
-      [1] this subject's spectrogram tensor, shape (*, 65, 73)
-      [2] this subject's time tensor, shape (*,)
-      [3] this subject's psg labels tensor, shape (*,)
-    """
+    Yields: (subject number excluded from training split, training_split_data, testing_split_data)
+    Where:
+    training_split_data:
+      [0] if with_time, a 2-tuple:
+        [0] training spectrograms tensor, shape (*, 65, 73)
+        [1] training time tensor, shape (*,)
+        (!) if not with_time, just training spectrograms
+      [1] training psg labels tensor, shape (*,)
+    testing_split_data is similar.
+
+    See data_yielder for an explanation of the parameters.
+     """
     data_list = [d for d in data_yielder(data_path = data_path, exclude = exclude, nn = False)]  
     
     for i, d in enumerate(data_list):
@@ -139,6 +153,209 @@ def split_yielder(data_path = None, exclude = None, with_time = True):
     
         yield (d[0], training_split_data, testing_split_data)
       
+
+
+
+def eval_nn(subject_number, nn_base_name, with_time = False, data_path = None, neural_path = None):
+    """
+    Helper function to load one subject's data and the neural net training on the complementary data, 
+    then evaluate the neural network on the testing data. Returns a numpy array with shape
+    (N, n_classes) where the subject's PSG has shape (N, 1) or (N, ).
+
+    data_path and neural_path default to ../data/pickles and ../data/neural
+    """
+    # Set up paths
+    if data_path is None:
+        data_path = "data/pickles/"
+    else:
+        if not isinstance(data_path, str):
+            raise ValueError("data_path must be a string.")
+
+        if data_path[-1] != "/":
+            data_path += "/"
+
+    if neural_path is None:
+        neural_path = "data/neural/"
+    else:
+        if not isinstance(neural_path, str):
+            raise ValueError("neural_path must be a string.")
+
+        if neural_path[-1] != "/":
+            neural_path += "/"
+
+    # Load the neural network
+    try:
+        nn = tf.keras.models.load_model(neural_path + subject_number + nn_base_name)
+    except:
+        print("There was an error opening the trained neural network.")
+        return
+
+    # last layer of network has shape (None, n_classes)
+    n_classes = nn.output.shape[1]
+
+    # Load the subject's data, throwing exceptions if the pickles are missing or unopenable.
+    ## Load spectrograms
+    try:
+        with open(data_path + subject_number + "/spectros.pickle", "rb") as f:
+            spectros = pickle.load(f)
+    except:
+        print("There was an error opening the pickled spectrograms for {}.".format(subject_number))
+        return
+    ## Load labels
+    try:
+        with open(data_path + subject_number + "/psg.pickle", "rb") as f:
+            psg = pickle.load(f)
+
+        psg = sleep_classes(psg, n_classes)
+
+    except:
+        print("There was an error opening the pickled PSG for {}.".format(subject_number))
+        return
+
+    # Load time if we want that, and set up the testing data.
+    if with_time is True:
+        try:
+            with open(data_path + subject_number + "/time.pickle", "rb") as f:
+                time = pickle.load(f)
+        except:
+            print("There was an error opening the pickled times for {}.".format(subject_number))
+            return
+
+        testing_X = [spectros, time]
+
+    else:
+        testing_X = spectros
+
+    
+
+    return nn.predict(testing_X)
+
+def pr_roc(subject_number, nn_base_name, pos_class = 0, with_time = False, data_path = None, neural_path = None, mode = "roc"):
+    """
+    Helper function to load one subject's data and the neural net training on the complementary data, 
+    then evaluate the performance of this network via PR or ROC curve, depending on mode. 
+
+    When pos_class is an int, returns a tuple suitable for plotting, interpolation, and pointwise averaging:
+        (false positive rate, true positive rate) 
+    OR
+        (recall, precision)
+    depending on if mode == "roc" or "pr", respectively.
+
+    When pos_class is list<int>, returns a list with len(pos_class) elements. 
+        The elements correspond to applying this function with the various pos_classes in the list.
+        E.g. if pos_class = [0,1] then returns 
+        [ pr_roc(..., pos_class = 0, ...), pr_roc(..., pos_class = 1, ...) ]
+
+    data_path and neural_path default to ../data/pickles and ../data/neural
+    """
+    if not (isinstance(pos_class, int) or isinstance(pos_class, list)):
+        raise ValueError("pos_class must be an int or list of ints")
+
+    # Set up paths
+    if data_path is None:
+        data_path = "data/pickles/"
+    else:
+        if not isinstance(data_path, str):
+            raise ValueError("data_path must be a string.")
+
+        if data_path[-1] != "/":
+            data_path += "/"
+
+    if neural_path is None:
+        neural_path = "data/neural/"
+    else:
+        if not isinstance(neural_path, str):
+            raise ValueError("neural_path must be a string.")
+
+        if neural_path[-1] != "/":
+            neural_path += "/"
+
+    # Load the subject's data, throwing exceptions if the pickles are missing or unopenable.
+    ## Load spectrograms
+    try:
+        with open(data_path + subject_number + "/spectros.pickle", "rb") as f:
+            spectros = pickle.load(f)
+    except:
+        print("There was an error opening the pickled spectrograms for {}.".format(subject_number))
+        return
+    ## Load labels
+    try:
+        with open(data_path + subject_number + "/psg.pickle", "rb") as f:
+            psg = pickle.load(f)
+    except:
+        print("There was an error opening the pickled PSG for {}.".format(subject_number))
+        return
+
+    # Load time if we want that, and set up the testing data.
+    if with_time is True:
+        try:
+            with open(data_path + subject_number + "/time.pickle", "rb") as f:
+                time = pickle.load(f)
+        except:
+            print("There was an error opening the pickled times for {}.".format(subject_number))
+            return
+
+        testing_X = [spectros, time]
+
+    else:
+        testing_X = spectros
+
+    
+    # Load the neural network
+    try:
+        nn = tf.keras.models.load_model(neural_path + subject_number + nn_base_name)
+    except:
+        print("There was an error opening the trained neural network.")
+
+    outcome = nn.predict(testing_X)
+
+    # Evaluate the outcomes, either operating on a single class if pos_class is an integer, or once per class given.
+    eval_fn = roc_curve if mode == "roc" else precision_recall_curve
+    if isinstance(pos_class, int):
+        x, y, _ = eval_fn(y_true = psg, y_score = outcome[:, pos_class], pos_label = pos_class)
+        return (x, y)
+
+    else: # isinstance(pos_class, list)
+        xys = [
+            eval_fn(
+                y_true = psg, 
+                y_score = outcome[:, p], 
+                pos_label = pos_class) 
+            for p in pos_class
+            ]
+
+        if len(xys) == 1:
+            xys = xys[0]
+
+        return xys
+
+
+
+def pointwise_avg(xy_pairs):
+    """
+    Given a list of (x, y) pairs, returns the pointwise average of
+    the y-values viewed as functions of their x-values. 
+
+    To accomplish this, we first take the refinement (union) of all the x's
+    by what is essentially a merge sort + unique of these arrays. 
+    """
+    if len(xy_pairs) == 1:
+        return xy_pairs
+
+    xs = [np.array(x) for x, y in xy_pairs]
+    
+    refined = xs[0]
+    for x in xs[1:]:
+        refined = np.hstack([refined, x])
+        refined.sort()
+        refined = np.unique(refined)
+    
+    # Interpolate to all be evaluated on the full refinement
+    ys = [np.interp(refined, x, y) for x, y in xy_pairs]
+
+    return np.mean(ys, axis=0)
+    
+
 """
 TODO(ericcanton): use pr_roc function instead of repeating code here
 Parameters:
@@ -158,74 +375,73 @@ Parameters:
 Returns:
     * (succeeded, evaluations, (fpr_interpolated, tpr_interpolated))
 """
-def pr_roc_from_path(data_yielder, title=None, pos_label=0, n_classes=2, label_names : dict=None, saveto=None, axis=None, mode="roc", from_logits = True):
+def pr_roc_from_path(
+        pickle_path,
+        neural_path,
+        nn_base_name,
+        title=None, 
+        pos_class = 0,
+        n_classes=2, 
+        label_names : dict = None, 
+        saveto=None, 
+        from_logits = True,
+        with_time = False):
+
+    import re
+
     if mode not in ['pr', 'roc']:
-        raise ValueError("mode must be in ['pr', 'roc'].")
+        raise ValueError("mode must be 'pr' OR 'roc'.")
 
-    #######################################################################################
-    #######################################################################################
-    # Evaluate the left-one-out test samples on the trained neural networks
-    evaluations = []
-    pr_rocs = []
-    failed = []
-    succeeded = []
-    for subject, sp, time, psg, nn in data_yielder:
-        print(subject)
-        psg = sleep_classes(psg, n_classes)
+    # Get list of NNs we have trained. These are the only ones we can evalutate!
+    nns = next(os.walk(neural_path))[-1]
+    subs = [re.search("[0-9]*", nn).group(0) for nn in nns]
 
-        if from_logits:
-            evaluations.append(logits_to_proba(nn.predict([sp, time])))
-        else:
-            evaluations.append(nn.predict([sp, time]))
-    #    print("Evaluated...")
+    # Make all of the plots. 
+    # We want to make an ROC and a PR plot for each class in pos_label.
+    # First make sure we have a list
+    if isinstance(pos_label, int):
+        pos_label = [pos_label]
 
-        # roc_curve(...) ~~> (FPR, TPR, thresholds)
-        # evaluations[-1] is a numpy array with shape (*, 2)
-        #   ~~~ evaluations[-1][i] == (Pr(y[i] = 0), Pr(y[i] = 1))
-        #   Thus, we use evaluations[-1][:, pos_label] to get a vector of probabilites of y[i] == pos_label
-        try:
-            if mode == "roc":
-                pr_rocs.append(roc_curve(y_true = psg, y_score=evaluations[-1][:,pos_label], pos_label = pos_label))
-            else: 
-                pr_rocs.append(precision_recall_curve(psg, evaluations[-1][:, pos_label], pos_label=pos_label))
+#    pr_roc_plots = {
+#        'pr': [pr_roc(
+#                subject_num = s, 
+#                nn_base_name = nn_base_name, 
+#                pos_class = pos_class,
+#                with_time = with_time, 
+#                data_path = pickle_path, 
+#                neural_path = neural_path, 
+#                mode = 'pr')
+#            for s in subs],
+#        'roc': [pr_roc(
+#                subject_num = s, 
+#                nn_base_name = nn_base_name, 
+#                pos_class = pos_class,
+#                with_time = with_time, 
+#                data_path = pickle_path, 
+#                neural_path = neural_path, 
+#                mode = 'roc')
+#            for s in subs]
+#    }
+    
+    # Get list of per-class probability tensors
+    outcomes = [eval_nn(
+        subject_num = s,
+        nn_base_name = nn_base_name,
+        n_classes = n_classes,
+        with_time = with_time,
+        data_path = pickle_path,
+        neural_path = neural_path)
+        for s in subs]
 
-            succeeded.append(subject)
-        except:
-            failed.append(subject)
-            continue
 
-    if len(failed) > 0:
-        print("There were class issues for:")
-        print(failed)
-    if len(succeeded) == 0:
-        return evaluations
 
-    #######################################################################################
-    #######################################################################################
-    # Adjust the ROC outcomes so they're all the same length. That way, we can average them
-    # each roc in pr_rocs has the form
-    # roc == (FPR, TPR, thresholds)
-    max_thresh_len = max([len(roc[2]) for roc in pr_rocs]) # max length of fpr vector
-    fpr_interpolated = np.linspace(0, 1, max_thresh_len) # thresholds vector, for interpolation
 
-    ## rocs_np[split #, threshold[j], fpr 0 / tpr 1]
-    tpr_interpolated = np.zeros((len(pr_rocs), max_thresh_len))
-    for j in range(len(pr_rocs)):
-        ## pr_rocs[j] == (fpr, tpr, thresholds)
-        tpr_interpolated[j] = np.interp(x =fpr_interpolated, xp=pr_rocs[j][0], fp=pr_rocs[j][1]) 
+    print("per-class (x,y) pairs created, where (x,y) = (FPR, TPR) or = (recall, precision).")
 
-    roc_averaged = np.mean(tpr_interpolated, axis=0, dtype=np.float64)
-
-    if axis == None:
-        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(20, 10))
-    else:
-        ax = axis
-
-    ax.plot(fpr_interpolated, roc_averaged, c='#449deb', lw=2)
-    for j in range(len(pr_rocs)):
-        ax.plot(pr_rocs[j][0], pr_rocs[j][1], alpha=0.3, c='orange')
-
+    fig, ax = plt.subplots(nrows=n_classes, ncols=2, figsize=(10*n_classes, 20))
     ax.grid(True)
+
+    
 
     ax.margins(0.01)
     
