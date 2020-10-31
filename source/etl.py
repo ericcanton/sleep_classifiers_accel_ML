71c45eaedf0aa220423bb6b7051bdd4030e39b4d
ericcanton <57836255+ericcanton@users.noreply.github.com>
2020-10-31 10:40:35 -0500

    Add Apache 2.0 notices to etl and evaluator scripts.

--- source/etl.py
+++ source/etl.py
@@ -1,6 +1,8 @@
 """
 ETL and feature extraction functions for the sleep_classifiers_ML project.
 
+Released under Apache 2.0 License. See extended notice at the end of this file.
+
 Starts with some very short functions loading data from CSV into numpy arrays,
 taking magnitude of (x,y,z) coordinates, and taking numerical derivatives. 
 
@@ -466,3 +468,19 @@ Arguments:
 
     now = time.time()
     print("(%9.4f) Complete!\n" % (now - start))
+
+"""
+Copyright 2020 Eric Canton (ericcanton)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
--- source/evaluator.py
+++ source/evaluator.py
@@ -1,5 +1,9 @@
 """ 
-Evaluation scripts. There are four functions defined here:
+Evaluation scripts. 
+
+Released under Apache 2.0 License. See extended notice at the end of this file.
+
+There are several functions defined here:
     1. logits_to_proba
     2. data_yielder
     3. split_yielder
@@ -540,3 +544,20 @@ def get_probabilities(data_yielder, n_classes=2, from_logits=True):
 #                
 #            axs[i][j].set_title(title)
 #    plt.tight_layout()
+
+"""
+Copyright 2020 Eric Canton (ericcanton)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
